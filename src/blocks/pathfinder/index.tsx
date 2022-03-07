98f9f6f1bd944d9dc97198a474b9e7dad2ba92ec
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2022-03-07 20:02:08 +0100

    Add monaco to block

--- src/blocks/pathfinder/index.tsx
+++ src/blocks/pathfinder/index.tsx
@@ -1,31 +1,53 @@
-import { FileBlockProps, getLanguageFromFilename } from "@githubnext/utils";
+import {
+  FileBlockProps,
+  FileContext,
+  getLanguageFromFilename,
+} from "@githubnext/utils";
 import "./index.css";
+import Editor, {
+  DiffEditor,
+  useMonaco,
+  loader,
+  Monaco,
+} from "@monaco-editor/react";
+import {
+  editor,
+  IMarkdownString,
+  languages,
+  Position,
+  Uri,
+} from "monaco-editor";
+import { useEffect } from "react";
 
 export default function (props: FileBlockProps) {
   const { context, content, metadata, onUpdateMetadata } = props;
   const language = Boolean(context.path)
-    ? getLanguageFromFilename(context.path)
+    ? context.path.endsWith(".rs")
+      ? "rust"
+      : getLanguageFromFilename(context.path).toLowerCase()
     : "N/A";
 
+  const monaco: Monaco | null = useMonaco();
+  console.log(language);
+
+  useEffect(() => {
+    if (monaco) {
+      // monaco.languages.registerHoverProvider("rust", hoverProvider);
+      // monaco.languages.registerDefinitionProvider("rust", definitionProvider);
+    }
+  }, [monaco]);
+
   return (
-    <div className="Box m-4">
-      <div className="Box-header">
-        <h3 className="Box-title">
-          File: {context.path} {language}
-        </h3>
-      </div>
-      <div className="Box-body">
-        Metadata example: this button has been clicked{" "}
-        <button
-          className="btn"
-          onClick={() =>
-            onUpdateMetadata({ number: (metadata.number || 0) + 1 })
-          }
-        >
-          {metadata.number || 0} times
-        </button>
-        <pre className="mt-3 p-3">{content}</pre>
-      </div>
-    </div>
+    <Editor
+      height="90vh"
+      defaultLanguage={language}
+      defaultValue={content}
+      options={{
+        readOnly: true,
+      }}
+      beforeMount={async (monaco: Monaco) => {
+        // return await beforeMountHandler(monaco, context);
+      }}
+    />
   );
 }
